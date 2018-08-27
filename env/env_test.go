330b9025001dc976ac1ec4feb4661de034ee4641
David Zhang <crispgm@gmail.com>
2018-08-27 16:43:34 +0800

    Tests IsFuncs

--- env/env_test.go
+++ env/env_test.go
@@ -15,3 +15,16 @@ func TestNewDefEnv(t *testing.T) {
 		t.Error("NewDefaultEnv failed")
 	}
 }
+
+func TestIsFunc(t *testing.T) {
+	env := NewDefaultEnv()
+	if env.IsStaging() != false {
+		t.Error("IsStaging test failed")
+	}
+	if env.IsDevelopment() != false {
+		t.Error("IsDevelopment test failed")
+	}
+	if env.IsProduction() != false {
+		t.Error("IsProduction test failed")
+	}
+}
